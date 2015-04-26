db4ec4a01701597eb31a9f813f20bef6c10cb2cc
a1656856 <julio.v.bernardi@hotmail.com>
2015-04-26 20:51:15 -0300

    Update Pratica31.java

--- src/main/java/Pratica31.java
+++ src/main/java/Pratica31.java
@@ -1,5 +1,4 @@
-import java.util.Date;
-import java.util.GregorianCalendar;
+import static java.lang.Math.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,43 +9,19 @@ import java.util.GregorianCalendar;
  *
  * @author Julio
  */
-public class Pratica31 {
-    private static String meuNome = "Julio Vancini Bernardi";
-    private static GregorianCalendar dataNascimento = new GregorianCalendar(1992, 5, 24);
-    private static Date inicio, fim;
-    
+public class Pratica32 {
     public static void main(String[] args) {
-        inicio = new Date();
-        
-        String maiusculo;
-        maiusculo = meuNome.toUpperCase();
-        
-        System.out.println(maiusculo);
-        
-        String nomeFormatado;
-        
-        nomeFormatado = meuNome.substring(14, 15).toUpperCase() +
-                meuNome.substring(15).toLowerCase() + ", "
-                + meuNome.substring(0, 1).toUpperCase() + ". "
-                + meuNome.substring(6, 7).toUpperCase() + ".";
-        
-        System.out.println(nomeFormatado + "\n");
-        
-        long dias;
-        
-        dias = (inicio.getTime() - dataNascimento.getTimeInMillis())
-        /(1000*3600*24);
-        
-        System.out.println(dias);
-        
-        fim = new Date();
-        
-        long ms = fim.getTime() - inicio.getTime();
-        
-        System.out.println(ms);
-        
+        /* Dentro do método main faça uma chamada para a função densidade, 
+        passando valores para x, media e desvio de -1, 67 e 3 respectivamente. 
+        Exiba o valor calculado. Verifique se o valor calculado está correto.*/
+        System.out.println(densidade(-1, 67, 3));
+    }
+    
+    public static double densidade(double x, double media, 
+            double desvio) {
+        double d = (exp(-0.5*(pow((x - media)/desvio, 2))))/(sqrt(2*PI)*desvio);
+        return d;
     }
 }
-
 
 
